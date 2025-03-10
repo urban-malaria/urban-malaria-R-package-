256f9605d86589aadefff08dc43c7ba84fc1271b
Grace Legris <gracebea@gmail.com>
2025-03-10 15:33:40 -0500

    remove output_dir as it's not needed

--- example_final_call.R
+++ example_final_call.R
@@ -1,5 +1,5 @@
 
-# Directories
+# directories
 Drive <- file.path(gsub("[\\]", "/", gsub("Documents", "", gsub("OneDrive", "", Sys.getenv("HOME")))))
 Drive <- file.path(gsub("[//]", "/", Drive))
 DriveDir <- file.path(Drive, "Urban Malaria Proj Dropbox", "urban_malaria")
@@ -8,14 +8,12 @@ DataDir <- file.path(DriveDir, "data")
 StateShpDir <- file.path(DriveDir, "data/nigeria/nigeria_shapefiles/shapefiles/ShinyApp_shapefiles/all_reprioritization_nmep_states/STATES")
 ITNDir <- file.path(DataDir, "nigeria/ITN_distribution")
 RastersDir <- file.path(DriveDir, "data/nigeria/Raster_files")
-MapOutputDir <- file.path(DriveDir, "projects/urban_microstratification/NetSmartR/outputs")
 
-# Example final function call
+# example final function call
 results <- reprioritize(
   state_name = "Delta",
   shapefile_path = file.path(StateShpDir, "Delta/Delta_State.shp"),
   tpr_data_path = file.path(DataDir, "nigeria/nigeria_hmis/TPR data for selected NMEP prioritized states/Deltatpr.csv"),
-  output_dir = file.path(DriveDir, "projects/urban_microstratification/NetSmartR/extractions"),
   itn_dir = file.path(ITNDir, "pbi_distribution_Delta.xlsx"),
   raster_paths = list(
     h2o_distance_path = file.path(RastersDir, "distance_to_water_bodies/distance_to_water.tif"),
@@ -26,7 +24,7 @@ results <- reprioritize(
   ),
   risk_factors = c("distance_to_water", "mean_EVI", "flood", "u5_tpr_rdt"),
   urban_data_path = file.path(DataDir, "nigeria/urban_percentage/delta_urban_percentage.geojson"),
-  map_output_dir = file.path(MapOutputDir),
+  map_output_dir = file.path(DriveDir, "projects/urban_microstratification/NetSmartR/outputs"),
   include_settlement_type = "Yes",
   include_u5_tpr_data = "Yes",
   scenarios = c(20, 75),
